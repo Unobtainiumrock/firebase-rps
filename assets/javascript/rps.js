5b51bf15ac0b9524e0d3a8fc2d69c06c6bbd53f4
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-05-08 20:47:06 -0700

    Add base chat functionality, add tracking of users connected w/ a cap at 2

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function() {
-
   // Define Firebase
   var config = {
     apiKey: "AIzaSyDeMXQCMoGam_N4-TjyuwTykp4oHJLP4F0",
@@ -10,55 +9,90 @@ $(document).ready(function() {
     storageBucket: "to-do-list-802bc.appspot.com",
     messagingSenderId: "1096472633358"
   };
-  
-  // Initialize firebase app with configuration
   firebase.initializeApp(config);
 
-  // Short hands
+  // Assign the reference to the database to a variable named 'database'
   var database = firebase.database();
+  
+  
   var usernameInput = $('#username');
   var textInput = $('#text');
   var postButton = $('#post');
+  var peopleConnected = 0;
 
-  // Start Game
+  // Ref to where we will store connections
+  var connectionsRef = database.ref("/connections");
+  //Provided by Firebase
+  var connectedRef = database.ref(".info/connected");
+  // Start game
   initializeGame();
 
   // Save useful references for using throughout the app
   var playerRef = database.ref('players');
   var playerOneRef = playerRef.child('player1');
   var playerTwoRef = playerRef.child('player2');
+  var msgRef = database.ref('/messages');
 
 
-  // console.log(`Player Ref: ${playerRef}`);
-  // console.log(`Player One Ref: ${playerOneRef}`);
-  // console.log(`Player Two Ref: ${playerTwoRef}`);
+  // Firebase data listeners
+  connectedRef.on('value', function(snap) {
 
+    // If they are connected..
+    if (snap.val()) {
+      // If the game isn't already full
+      if(peopleConnected < 2) {
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
+      }
+    }
+  });
 
-  // Firebase listeners
+  connectionsRef.on('value',function(snap) {
+    peopleConnected = snap.numChildren();
 
-  playerOneRef.on('child_changed', function(data) {
+    $('#people-connected').text(peopleConnected);
+  })
+
+  playerOneRef.on('child_changed', function(snap) {
     // Log Player 1 changes as they occur
-    console.log(`Val:${data.val()} Key: ${data.key}`);
+    console.log(`Val:${snap.val()} Key: ${snap.key}`);
 
     // If a name is being submitted, change the button data-attr, so
     // that the second person who picks their name second is writing their
     // name to the player two area of firebase
-    if(data.key === 'name') {
+    if(snap.key === 'name') {
       // Change button data attributes
       $('#add-user').attr('data-player','player2');
     }
 
   })
 
-
-  playerTwoRef.on('child_changed', function(data) {
+  playerTwoRef.on('child_changed', function(snap) {
     // Log Player 2 changes as they occur
-    console.log(`Val:${data.val()} Key: ${data.key}`);
+    console.log(`Val:${snap.val()} Key: ${snap.key}`);
 
     // Hide name submission after both players pick their name
-    if(data.key === 'name') {
+    if(snap.key === 'name') {
       $('#name-submit').hide();
     }
+  })
+
+  msgRef.on('child_added',function(snap) {
+    var msgObj = snap.val();
+    console.log(msgObj);
+    // msg user
+
+    var msgDiv = $('<div>');
+    var user = $('<b>');
+    var message = $('<p>');
+    user.text(msgObj.user);
+    message.text(msgObj.msg);
+    msgDiv.append(user);
+    msgDiv.append(message);
+
+    $('#results').append(msgDiv);
   })
 
 
@@ -67,14 +101,15 @@ $(document).ready(function() {
     e.preventDefault();
     var name = $('#name-input').val().trim();
     var playerNumber = $('#add-user').attr('data-player');
+    var playerRef = database.ref('players'); 
     var thisPlayer = playerRef.child(playerNumber);
     thisPlayer.update({ name });
   })
 
   postButton.on('click',function() {
-    var msgUser = usernameInput.val();
-    var msgText = textInput.val();
-    database.ref('/messages').set(msgUser + " says: " + msgText);
+    var user = usernameInput.val();
+    var msg = textInput.val();
+    database.ref('/messages').push({user, msg});
     textInput.val('');
   })
 
@@ -82,6 +117,10 @@ $(document).ready(function() {
     var userChoice = $(this).attr('data-choice');
   })
 
+
+  $('#delete-db').on('click',function(e){
+    database.ref().set(null);
+  })
 
   // Functions
   function initializeGame() {
@@ -113,34 +152,4 @@ $(document).ready(function() {
     // database.ref('/messages').set({})
   }
 
-  // $.ajax({
-  //   url: playerRef,
-  //   method: 'GET'
-  // }).then(function(response) {
-  //   // console.log(response);
-  // })
-
-  // FIREBASE STUFF THAT MIGHT BE USEFUL
-  // The key of any non-root reference is the last token in the path
-  // var adaRef = firebase.database().ref("users/ada");
-  // var key = adaRef.key;  // key === "ada"
-  // key = adaRef.child("name/last").key;  // key === "last"
-
-
-  // Firebase watcher + initial loader HINT: .on("value")
-  // database.ref().on("value", function(snapshot) {
-  //   console.log(snapshot.val());
-
-  //   var formObj = snapshot.val();
-
-  //   $('#name-display').text(formObj.name);
-  //   $('#email-display').text(formObj.email);
-  //   $('#age-display').text(formObj.age);
-  //   $('#comment-display').text(formObj.comment);
-
-  // }, function(errorObject) {
-  //  console.log("The read failed: " + errorObject.code);
-  // });
-
-  // Create Error Handling
 })
